b63b4c719d773d427790bbf19f0cae0f4bdfc481
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-12-07 18:53:22 +0100

    Use a polars dataframe as datastorage for easy querying in the future. Load data in a background task

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use geo::vector::dataframe::Field;
 use gpui::{App, IntoElement, Window};
 use gpui_component::tab::{Tab, TabBar};
 use gpui_component::table::Table;
 use gpui_component::*;
+use polars::prelude::AnyValue;
 use std::path::PathBuf;
 
 use gpui::*;
@@ -11,20 +11,41 @@ use gpui_component::table::{Column, TableDelegate, TableState};
 mod appconfig;
 mod tableio;
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct TableLayer {
-    name: SharedString,
-    data: Vec<Vec<Option<Field>>>,
+    data: polars::frame::DataFrame,
     columns: Vec<Column>,
 }
 
+impl TableLayer {
+    pub fn create_column_info(&mut self) {
+        let schema = self.data.schema();
+        self.columns = schema
+            .iter()
+            .map(|(name, _dtype)| {
+                let name = SharedString::new(name.as_str());
+                Column {
+                    key: name.clone(),
+                    name,
+                    ..Default::default()
+                }
+            })
+            .collect();
+    }
+}
+
 impl TableDelegate for TableLayer {
+    fn loading(&self, _cx: &App) -> bool {
+        self.columns.is_empty()
+    }
+
     fn columns_count(&self, _: &App) -> usize {
+        debug_assert_eq!(self.data.shape().1, self.columns.len());
         self.columns.len()
     }
 
     fn rows_count(&self, _: &App) -> usize {
-        self.data.len()
+        self.data.shape().0
     }
 
     fn column(&self, col_ix: usize, _: &App) -> &Column {
@@ -38,24 +59,20 @@ impl TableDelegate for TableLayer {
         _: &mut Window,
         _: &mut gpui::Context<'_, TableState<Self>>,
     ) -> impl IntoElement {
-        let row = &self.data[row_ix];
-
-        match &row[col_ix] {
-            Some(field) => match field {
-                Field::String(s) => s.clone(),
-                Field::Integer(i) => i.to_string(),
-                Field::Float(f) => f.to_string(),
-                Field::Boolean(b) => b.to_string(),
-                Field::DateTime(naive_date_time) => naive_date_time.to_string(),
-            },
-            None => String::default(),
+        match self.data[col_ix].get(row_ix) {
+            Ok(AnyValue::String(str)) => str.into(),
+            Ok(val) => val.to_string(),
+            Err(_) => "ERR".into(),
         }
     }
 }
 
 pub struct TableView {
     active_tab: usize,
-    layers: Vec<Entity<TableState<TableLayer>>>,
+    data_path: Option<PathBuf>,
+    layer_names: Vec<SharedString>,
+    table: Entity<TableState<TableLayer>>,
+    _load_task: Task<()>,
 }
 
 impl TableView {
@@ -64,19 +81,52 @@ impl TableView {
     }
 
     fn new(path: Option<PathBuf>, window: &mut Window, cx: &mut gpui::Context<Self>) -> Self {
-        let layers = match path {
-            Some(path) => tableio::load_file(path)
-                .unwrap_or_default()
-                .into_iter()
-                .map(|layer| cx.new(|cx| TableState::new(layer, window, cx)))
-                .collect(),
-            None => Vec::new(),
-        };
+        let table = cx.new(|cx| TableState::new(TableLayer::default(), window, cx));
+
+        if let Some(path) = path {
+            if let Ok(layer_names) = tableio::layers_for_path(&path) {
+                let task = if let Some(current_layer) = layer_names.first() {
+                    Self::load_table_layer_task(path.clone(), current_layer.to_string(), cx)
+                } else {
+                    Task::ready(())
+                };
+
+                return Self {
+                    active_tab: 0,
+                    data_path: Some(path),
+                    table,
+                    layer_names,
+                    _load_task: task,
+                };
+            }
+        }
 
         Self {
             active_tab: 0,
-            layers,
+            data_path: None,
+            table,
+            layer_names: Vec::default(),
+            _load_task: Task::ready(()),
         }
+    }
+
+    fn load_table_layer_task(
+        path: PathBuf,
+        layer: String,
+        cx: &mut gpui::Context<Self>,
+    ) -> Task<()> {
+        cx.spawn(async move |this, cx| {
+            let layer_data = tableio::layer_data(&path, &layer).unwrap_or_default();
+
+            let _ = this.update(cx, |this, cx| {
+                this.table.update(cx, |table, cx| {
+                    table.delegate_mut().data = layer_data;
+                    table.delegate_mut().create_column_info();
+                    table.refresh(cx);
+                });
+                cx.notify();
+            });
+        })
     }
 
     fn render_tab_content(
@@ -84,22 +134,17 @@ impl TableView {
         _window: &mut Window,
         _cx: &mut gpui::Context<Self>,
     ) -> impl IntoElement {
-        self.layers.get(self.active_tab).map_or_else(
-            || div().child("No layer selected"),
-            |layer| {
-                div()
-                    .flex()
-                    .flex_1()
-                    .size_full()
-                    .child(Table::new(layer).stripe(true).xsmall())
-            },
-        )
+        div()
+            .flex()
+            .flex_1()
+            .size_full()
+            .child(Table::new(&self.table).stripe(true).xsmall())
     }
 }
 
 impl Render for TableView {
     fn render(&mut self, window: &mut Window, cx: &mut gpui::Context<Self>) -> impl IntoElement {
-        if self.layers.is_empty() {
+        if self.layer_names.is_empty() {
             return div().grid().size_full().content_center().child(
                 div()
                     .font_bold()
@@ -113,12 +158,14 @@ impl Render for TableView {
             .selected_index(self.active_tab)
             .on_click(cx.listener(|view, index, _, cx| {
                 view.active_tab = *index;
+                let path = view.data_path.clone().unwrap();
+                let layer_name = view.layer_names[*index].to_string();
+                view._load_task = Self::load_table_layer_task(path, layer_name, cx);
                 cx.notify();
             }));
 
-        for layer in &self.layers {
-            let x = layer.read(cx);
-            tab_bar = tab_bar.child(Tab::new().label(x.delegate().name.clone()));
+        for layer in &self.layer_names {
+            tab_bar = tab_bar.child(Tab::new().label(layer.clone()));
         }
 
         v_flex()
--- src/tableio.rs
+++ src/tableio.rs
@@ -1,56 +1,71 @@
 use anyhow::Result;
 use gpui::SharedString;
-use gpui_component::table::Column;
-use std::path::PathBuf;
+use std::path::{Path};
 
 use geo::vector::dataframe::{DataFrameOptions, HeaderRow, create_dataframe_reader};
 
-use crate::TableLayer;
+pub fn layers_for_path(path: &Path) -> Result<Vec<SharedString>> {
+    let reader = create_dataframe_reader(path)?;
+    Ok(reader.layer_names()?.iter().map(Into::into).collect())
+}
 
-pub fn load_file(path: PathBuf) -> Result<Vec<TableLayer>> {
-    let mut layers = Vec::new();
-
-    let mut reader = create_dataframe_reader(&path)?;
-    for layer in reader.layer_names()? {
-        let opts = DataFrameOptions {
-            layer: Some(layer.clone()),
+pub fn layer_data(path: &Path, layer: &str) -> Result<polars::frame::DataFrame> {
+    let df = geo::vector::dataframe::polars::read_dataframe(
+        path,
+        &DataFrameOptions {
+            layer: Some(layer.to_string()),
             header_row: HeaderRow::Auto,
             ..Default::default()
-        };
+        },
+    )?;
 
-        let columns = reader
-            .schema(&opts)?
-            .fields
-            .iter()
-            .map(|f| {
-                let name = SharedString::new(f.name());
-                Column {
-                    key: name.clone(),
-                    name,
-                    ..Default::default()
-                }
-            })
-            .collect();
-
-        let mut column_data = Vec::new();
-        reader.iter_rows(&opts)?.for_each(|row| {
-            column_data.push(
-                row.fields
-                    .into_iter()
-                    .map(|field| match field {
-                        Ok(field) => field,
-                        Err(_) => None,
-                    })
-                    .collect(),
-            );
-        });
-
-        layers.push(TableLayer {
-            name: layer.into(),
-            columns,
-            data: column_data,
-        });
-    }
-
-    Ok(layers)
+    Ok(df)
 }
+
+// pub fn load_file(path: PathBuf) -> Result<Vec<TableLayer>> {
+//     let mut layers = Vec::new();
+
+//     let mut reader = create_dataframe_reader(&path)?;
+//     for layer in reader.layer_names()? {
+//         let opts = DataFrameOptions {
+//             layer: Some(layer.clone()),
+//             header_row: HeaderRow::Auto,
+//             ..Default::default()
+//         };
+
+//         let columns = reader
+//             .schema(&opts)?
+//             .fields
+//             .iter()
+//             .map(|f| {
+//                 let name = SharedString::new(f.name());
+//                 Column {
+//                     key: name.clone(),
+//                     name,
+//                     ..Default::default()
+//                 }
+//             })
+//             .collect();
+
+//         let mut column_data = Vec::new();
+//         reader.iter_rows(&opts)?.for_each(|row| {
+//             column_data.push(
+//                 row.fields
+//                     .into_iter()
+//                     .map(|field| match field {
+//                         Ok(field) => field,
+//                         Err(_) => None,
+//                     })
+//                     .collect(),
+//             );
+//         });
+
+//         layers.push(TableLayer {
+//             name: layer.into(),
+//             columns,
+//             data: column_data,
+//         });
+//     }
+
+//     Ok(layers)
+// }
