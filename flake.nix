5babf59cde832024880965daa572ea2d2878e7be
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-04 23:58:01 +0200

    Rename Nix package and update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "tables";
+  description = "tabulite";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
@@ -23,13 +23,24 @@
         packages = {
           # regular, host-native build (dynamic)
           default = pkgs.rustPlatform.buildRustPackage {
-            pname = "tables";
+            pname = "tabulite";
             version = "1.0.0";
 
             src = ./.;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [
+              pkgs.fontconfig
+              pkgs.libxcb
+              pkgs.libxkbcommon
+            ];
 
             # assuming you have a Cargo.lock
-            cargoLock.lockFile = ./Cargo.lock;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "geo-2026.9.2" = "sha256-mr0kUqgcLAKGg8T3tB0Jw9PSeE1MxADWij9CqQ+FE+g=";
+              };
+            };
           };
         }
         // (
@@ -37,12 +48,23 @@
           if pkgs.stdenv.isLinux then
             {
               static = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
-                pname = "tables";
+                pname = "tabulite";
                 version = "1.0.0";
 
                 src = ./.;
+                nativeBuildInputs = [ pkgs.pkg-config ];
+                buildInputs = [
+                  pkgs.fontconfig
+                  pkgs.libxcb
+                  pkgs.libxkbcommon
+                ];
 
-                cargoLock.lockFile = ./Cargo.lock;
+                cargoLock = {
+                  lockFile = ./Cargo.lock;
+                  outputHashes = {
+                    "geo-2026.9.2" = "sha256-mr0kUqgcLAKGg8T3tB0Jw9PSeE1MxADWij9CqQ+FE+g=";
+                  };
+                };
               };
             }
           else
