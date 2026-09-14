72f66562ca9f6eac6748d252043739be1a03ad49
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-14 23:24:42 +0200

    Update Nixpkgs and Geo source hash

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
             cargoLock = {
               lockFile = ./Cargo.lock;
               outputHashes = {
-                "geo-2026.9.14" = "sha256-mr0kUqgcLAKGg8T3tB0Jw9PSeE1MxADWij9CqQ+FE+g=";
+                "geo-2026.9.14" = "sha256-Gxf8s6zCalGUNitQODAXSgKu3Jai0ALY4QhwzbBJTVw=";
               };
             };
           };
@@ -83,7 +83,7 @@
                 cargoLock = {
                   lockFile = ./Cargo.lock;
                   outputHashes = {
-                    "geo-2026.9.2" = "sha256-mr0kUqgcLAKGg8T3tB0Jw9PSeE1MxADWij9CqQ+FE+g=";
+                    "geo-2026.9.14" = "sha256-Gxf8s6zCalGUNitQODAXSgKu3Jai0ALY4QhwzbBJTVw=";
                   };
                 };
               };
